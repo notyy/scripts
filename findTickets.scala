e56ed481c8170636c1b9c723b8ccf5e1f41b8037
notyy <notyycn@gmail.com>
2012-08-30 09:57:15 +0800

    add a script used to analize git log

---
+++ findTickets.scala
@@ -0,0 +1,8 @@
+#!/bin/sh                                                                                                                                                  exec scala -nc "$0" "$@"
+!#
+
+import scala.io._
+
+val source = if(args.length>0) Source.fromFile(args(0)).getLines else Source.stdin.getLines
+val pattern = """([a-zA-Z]+)-(\d+)""".r
+source.flatMap(pattern.findFirstIn).toList.distinct.foreach(println)

f8c04676553e7b09862c78972936570a9ccfc2e3
notyy <notyycn@gmail.com>
2012-10-15 16:07:34 +0800

    allow it to process multiple files

--- removeBranch.scala
+++ removeBranch.scala
@@ -4,9 +4,14 @@ exec scala -nc "$0" "$@"
 
 import scala.io._
 
+import java.io._
+
+def writeToFile(f: java.io.File)(data: List[String]) {
+  val p = new java.io.PrintWriter(f)
+  try { data.foreach(p.println) } finally { p.close() }
+}
+
 def processChooseBlock(block: List[String]): List[String] = {
-  println("processing choose block:")
-  block.foreach(println)
   val rs = if(block(1).contains("<c:when test=\"${ mobileLevel eq 'high' }\">")) {
     if(block.find(_.contains("<choose>")).isEmpty){
       block.dropWhile(x => !(x.contains("<c:when test=\"${ mobileLevel eq 'high' }\">"))).
@@ -16,16 +21,17 @@ def processChooseBlock(block: List[String]): List[String] = {
   rs.tail.reverse
 }
 
-val fileName = if(args.length>0) args(0) else Source.stdin.getLines.next
-val source =  Source.fromFile(fileName).getLines
-println("processing " + fileName)
-val t = source.foldLeft((List[String](),List[String]())){
-  case ((target, block), s) if s.contains("<c:choose>") => (target, s ::block)
-  case ((target, block), s) if s.contains("</c:choose>") => (processChooseBlock((s::block).reverse) ++ target, Nil)
-  case ((target, block), s) if ! block.isEmpty => (target, s :: block)
-  case ((target, Nil), s) => (s:: target, Nil)
+def process(fileName: String) = {
+  //val fileName = if(args.length>0) args(0) else Source.stdin.getLines.next
+  val source =  Source.fromFile(fileName).getLines
+  println("processing " + fileName)
+  val t = source.foldLeft((List[String](),List[String]())){
+    case ((target, block), s) if s.contains("<c:choose>") => (target, s ::block)
+    case ((target, block), s) if s.contains("</c:choose>") => (processChooseBlock((s::block).reverse) ++ target, Nil)
+    case ((target, block), s) if ! block.isEmpty => (target, s :: block)
+    case ((target, Nil), s) => (s:: target, Nil)
+  }
+  writeToFile(new File(fileName))(t._1.reverse)
 }
 
-println("content of target:")
-t._1.reverse.foreach(println)
-println("process ok")
+args.foreach(process)
